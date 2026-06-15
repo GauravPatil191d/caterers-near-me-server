2c0a9b75d52d922dc97f1c7f611f3b55da1f36c2
Corbin Technology <techwork@corbintech.co.in>
2026-06-15 14:37:39 +0530

    feat:'the new changes done here okay"

--- build/index.js
+++ build/index.js
@@ -5,16 +5,19 @@ import cors from "cors";
 import { connectMongo } from "./config/mongo.js";
 import caterer_router from "./modules/caterers/router/caterer_router.js";
 dotenv.config();
-const port = process.env.PORT;
+const port = process.env.PORT || 8000;
 const app = express();
 const httpServer = createServer(app);
-app.use(cors({
-    origin: ["http://localhost:3000"],
-    credentials: true,
-}));
-connectMongo();
+app.use(cors());
 app.use(express.json());
+app.get("/", (req, res) => {
+    res.json({
+        success: true,
+        message: "Backend Running",
+    });
+});
 app.use("/api", caterer_router);
+connectMongo();
 httpServer.listen(port, () => {
     console.log(`Server running on port ${port} and url http://localhost:${port}/`);
 });
